95f96e12bd40930772af8abd9d7bf3a51018443a
Alexei <alexei03091997@gmail.com>
2016-05-31 01:34:30 +0300

    забыл

---
+++ core/src/com/mygdx/game/MyGame.java
@@ -0,0 +1,11 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Screen;
+
+/**
+ * Created by Алексей on 31.05.2016.
+ */
+public abstract class MyGame extends Game {
+    public abstract void curScreen(Screen screen);
+}
